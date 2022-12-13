de909a213272f111b4fb50344bf76da13b338665
Filiberto_Castro <fcastrob@miumg.edu.gt>
2022-12-13 11:49:24 -0600

    apply rotation to the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,7 @@ public class PlayerController : MonoBehaviour
 {
     [Header("Player Setting")]
     public float moveSpeed = 0f;
+    public float rotationSpeed = 280.0f;
     private Rigidbody rb;
     private float horizontal;
     private float vertical;
@@ -41,8 +43,21 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate() 
     {
+        // Asignando direccion al jugador
         Vector3 moveDirection = Vector3.forward * vertical + Vector3.right * horizontal;
 
+        // Asigando rotacion del jugador
+        Vector3 projectedCameraForward = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up);
+        Quaternion rotationToCamera = Quaternion.LookRotation(projectedCameraForward, Vector3.up);
+
+        // Aplicando rotacion a la dirección
+        moveDirection = rotationToCamera * moveDirection;
+
+        // Rotando al jugador
+        Quaternion rotationToMoveDirection = Quaternion.LookRotation(moveDirection, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationToMoveDirection, rotationSpeed * Time.deltaTime);
+        
+        // Moviendo al jugador
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
     }
 
@@ -51,6 +66,7 @@ public class PlayerController : MonoBehaviour
         winTextObject.SetActive(true);
     }
 
+    // Detenctando collisiones
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.CompareTag("PickUp"))
         {
